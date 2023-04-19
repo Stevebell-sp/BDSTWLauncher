15f6a900c0efd6305dbb826ef71b9e84263b457a
Steve <ga88105@gmail.com>
2023-04-19 17:38:32 +0800

    Update to Electron 17, fix deleting drop-in mods from the settings view.

--- .history/app/assets/js/ipcconstants_20230418195049.js
+++ .history/app/assets/js/ipcconstants_20230418195049.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '96671da6-9bb9-4cc2-9709-8b5a8753ab9a'
+exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
 // SEE NOTE ABOVE.
 
 
