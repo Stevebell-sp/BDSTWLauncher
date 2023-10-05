8e28b5752fcba29e3b5b4e7ee4650b7311d9b53b
Steve <ga88105@gmail.com>
2023-10-05 09:55:31 +0800

    更改網址位置

--- .history/app/assets/js/distromanager_20231005094834.js
+++ .history/app/assets/js/distromanager_20231005094834.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://bdstw.org/public/modpack/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://modpack.bdstw.org/data/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
