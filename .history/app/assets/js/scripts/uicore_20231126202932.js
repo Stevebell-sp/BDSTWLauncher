f88b384c65eccc1ef5c9b45512ce6f6ce571a4dc
Steve <ga88105@gmail.com>
2023-11-26 23:41:45 +0800

    變更

--- .history/app/assets/js/scripts/uicore_20231126202932.js
+++ .history/app/assets/js/scripts/uicore_20231126202932.js
@@ -27,9 +27,9 @@ window.eval = global.eval = function () {
 
 // Display warning when devtools window is opened.
 remote.getCurrentWebContents().on('devtools-opened', () => {
-    console.log('%c此客戶端後台是作者排除BUG時會使用的工具', 'color: white; -webkit-text-stroke: 4px #a02d2a; font-size: 60px; font-weight: bold')
-    console.log('%c如果\'有人告訴你要在這裡貼上什麼東西\'那麼你正在受到詐騙', 'font-size: 16px')
-    console.log('%c除非您確切知道自己在做什麼\'否則請關閉此視窗', 'font-size: 16px')
+    console.log('%cThe console is dark and full of terrors.', 'color: white; -webkit-text-stroke: 4px #a02d2a; font-size: 60px; font-weight: bold')
+    console.log('%cIf you\'ve been told to paste something here, you\'re being scammed.', 'font-size: 16px')
+    console.log('%cUnless you know exactly what you\'re doing, close this window.', 'font-size: 16px')
 })
 
 // Disable zoom, needed for darwin.
@@ -49,7 +49,7 @@ if(!isDev){
                 loggerAutoUpdater.info('New update available', info.version)
                 
                 if(process.platform === 'darwin'){
-                    info.darwindownload = `https://github.com/Stevebell-sp/BDSTWLauncher/releases/download/v${info.version}/BDSTW-Launcher-setup--${info.version}${process.arch === 'arm64' ? '-arm64' : '-x64'}.dmg`
+                    info.darwindownload = `https://github.com/dscalzi/HeliosLauncher/releases/download/v${info.version}/Helios-Launcher-setup-${info.version}${process.arch === 'arm64' ? '-arm64' : '-x64'}.dmg`
                     showUpdateUI(info)
                 }
                 
