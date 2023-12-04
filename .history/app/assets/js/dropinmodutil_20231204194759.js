dbd4066a3e6a741422d56c5299a406ee8b7ad1dc
Steve <ga88105@gmail.com>
2023-12-04 20:55:10 +0800

    中文化

--- .history/app/assets/js/dropinmodutil_20231204194759.js
+++ .history/app/assets/js/dropinmodutil_20231204194759.js
@@ -156,7 +156,7 @@ exports.scanForShaderpacks = function(instanceDir){
     const shaderDir = path.join(instanceDir, SHADER_DIR)
     const packsDiscovered = [{
         fullName: 'OFF',
-        name: '關閉 (預設)'
+        name: 'Off (Default)'
     }]
     if(fs.existsSync(shaderDir)){
         let modCandidates = fs.readdirSync(shaderDir)
